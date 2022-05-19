f3bbbd2017d3cac6afed16cd6209b396d1b85c65
tresor-ruph <tresortek9@gmail.com>
2022-05-20 00:55:43 +0200

    parc_equipment module

---
+++ parc_equipment/models/maintenance_equipment.py
@@ -0,0 +1,54 @@
+from odoo import models, fields, _
+
+
+class ParcEquipment(models.Model):
+    _inherit = "maintenance.equipment"
+    _description = "Trane Parc Maintenance Equipment"
+    
+    origin_picking_ids = fields.Many2many("stock.picking", string="origin picking")
+    client_id = fields.Many2one("res.partner", string="Client & Address")
+    account_analystic_id = fields.Many2one("account.analytic.account", string="Analytic Account")
+    product_id = fields.Many2one("product.product", string="Product")
+
+    def action_open_view_document(self):
+        views = [
+            (self.env.ref('documents.document_view_kanban').id, 'kanban'),
+            (self.env.ref('documents.documents_view_list').id, 'tree'),       
+        ]
+        return {
+            'name': _('Document'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'kanban',
+            'res_model': 'documents.document',
+            'views': views,
+            'view_id': self.env.ref('documents.document_view_kanban').id,
+            'target': 'current',
+            'context': {
+                "search_default_partner_id": self.partner_id.id,
+                "default_partner_id": self.partner_id.id,
+                "searchpanel_default_folder_id": False
+            },
+        }
+
+    def action_open_view_stock(self):
+        operation_type_id = False
+        operation_type_code = self._context.get("picking_type_code")
+        operation_incomming = operation_type_code == "incoming"
+      
+        views = [
+            (self.env.ref('stock.stock_picking_kanban').id, 'kanban'),
+            (self.env.ref('stock.vpicktree').id, 'tree'),
+            (self.env.ref('stock.stock_picking_calendar').id, 'calendar'),
+            (self.env.ref('stock_enterprise.stock_map_view').id, 'map'), 
+            (self.env.ref('stock.view_picking_form').id, 'form'),       
+        ]
+        return {
+            'name': _('Receipt') if operation_incomming else _('Delivery'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'kanban',
+            'res_model': 'stock.picking',
+            'views': views,
+            'view_id': self.env.ref('stock.stock_picking_kanban').id,
+            'target': 'current',
+            'domain': [('id', 'in', self.origin_picking_ids.ids), ('picking_type_code', '=', operation_type_code)],
+        }     
---
+++ parc_equipment/models/stock_picking.py
@@ -0,0 +1,52 @@
+from odoo import models, api, fields, _
+
+
+class TraneStockPicking(models.Model):
+    _inherit = "stock.picking"
+    
+    def button_validate(self):
+        self.ensure_one()
+        res = super(TraneStockPicking, self).button_validate()
+        if self.picking_type_id.code == "incoming":
+            for move in self.move_ids_without_package:
+                equipment = self.env["maintenance.equipment"].search([('product_id', '=', move.product_id.id), ('origin_picking_ids', 'in', self.id)])
+                if not equipment:
+                    self.env["maintenance.equipment"].create(
+                        {
+                            'product_id': move.product_id.id,
+                            'name': move.product_id.name,
+                            'effective_date': move.date,
+                            'origin_picking_ids': [(4, self.id)],
+                            'account_analystic_id': move.analytic_account_line_id.id,
+                            'serial_no': len(move.lot_ids) > 0 and move.lot_ids[:1].name,
+                        }
+                    )
+        elif self.picking_type_id.code == "outgoing":
+            for move in self.move_ids_without_package:
+                equipment_id = self.env["maintenance.equipment"].search([('product_id', '=', move.product_id.id)], limit=1)
+                if equipment_id:
+                    equipment_id.write(
+                        {
+                            'client_id': self.partner_id,
+                            'assign_date': self.scheduled_date,
+                            'origin_picking_ids': [(4, self.id)],
+                        }
+                    )
+        return res
+
+    def action_open_view_equipment(self):
+        views = [
+            (self.env.ref('maintenance.hr_equipment_view_kanban').id, 'kanban'),
+            (self.env.ref('maintenance.hr_equipment_view_tree').id, 'tree'),
+            (self.env.ref('maintenance.hr_equipment_view_form').id, 'form'),       
+        ]
+        return {
+            'name': _('Equipment'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'kanban',
+            'res_model': 'maintenance.equipment',
+            'views': views,
+            'view_id': self.env.ref('maintenance.hr_equipment_view_kanban').id,
+            'target': 'current',
+            'domain': [('origin_picking_ids', 'in', self.id)],
+        }
