9954e8e15afd96338351896dc32308b8b4e7e690
tresor-ruph <tresortek9@gmail.com>
2022-05-23 22:26:48 +0200

    m

--- parc_equipment/models/stock_picking.py
+++ parc_equipment/models/stock_picking.py
@@ -21,7 +21,7 @@ class TraneStockPicking(models.Model):
         elif self.picking_type_id.code == "outgoing":
             for move in self.move_ids_without_package:
                 equipment_id = self.env["maintenance.equipment"].search([('product_id', '=', move.product_id.id), ('serial_no','=', move.lot_ids[:1].name) ], limit=1)
-                if equipment_id and not equipment_serial:
+                if equipment_id:
                     equipment_id.write(
                         {
                             'client_id': self.partner_id,
